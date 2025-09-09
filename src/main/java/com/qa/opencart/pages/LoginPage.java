978112d4aac76de01de4a4c9a852eb42680f19aa
HarishTakle34 <careerpathforu@gmail.com>
2025-09-09 10:46:58 +0530

    Created a new demo page and added the locator in the login page

--- src/main/java/com/qa/opencart/pages/LoginPage.java
+++ src/main/java/com/qa/opencart/pages/LoginPage.java
@@ -22,6 +22,8 @@ public class LoginPage {
 	 private By registerlink= By.linkText("Register");
 	 private By loginErrorMessage = By.cssSelector(".alert.alert-danger.alert-dismissible");
 	 
+	 private By textlocator= By.id("text");
+	 
 	 //page const....
 	 public LoginPage(WebDriver driver)
 	 {
