dae721b2ff369c1666ef29581086c8af6d1b0c88
HarishTakle34 <careerpathforu@gmail.com>
2025-09-12 12:44:17 +0530

    added the feature1.0

---
+++ src/main/java/com/qa/opencart/pages/Featur1.java
@@ -0,0 +1,11 @@
+package com.qa.opencart.pages;
+
+public class Featur1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is the Feature1.0 file ");
+	}
+
+}
