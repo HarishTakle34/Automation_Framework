9fe4a8909d8c0fd5f9392ac984b25cc70510fa03
HarishTakle34 <careerpathforu@gmail.com>
2025-09-12 12:46:19 +0530

    Adding the feature2.0

---
+++ src/main/java/com/qa/opencart/pages/Feature2.java
@@ -0,0 +1,10 @@
+package com.qa.opencart.pages;
+
+public class Feature2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("This is the feature 2.0 file");
+	}
+
+}
