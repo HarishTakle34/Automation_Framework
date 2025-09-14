db394c96a8b4b5c57e7a863d37b459d7008e972a
HarishTakle34 <careerpathforu@gmail.com>
2025-09-14 12:57:11 +0530

    i-20 in Home

---
+++ src/main/java/com/qa/opencart/pages/Home.java
@@ -0,0 +1,13 @@
+package com.qa.opencart.pages;
+
+public class Home {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i=20;
+		System.out.println(i);
+		
+	}
+
+}
